aba1f5e51d42748b0eb261fbb56b279a12a9f499
Valeriy73 <turlera1@ukr.net>
2018-01-21 13:23:44 +0200

    js code simple validation

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Static server
 gulp.task('server', function() {
@@ -38,6 +41,22 @@ gulp.task('styles:compile', function () {
 	}))
 	.pipe(rename('main.min.css'))
     .pipe(gulp.dest('build/css'));
+});
+
+// JS
+
+gulp.task('js', function () {
+  return gulp.src([
+      'source/js/init.js',
+      'source/js/validation.js',
+  		'source/js/form.js',
+  		'source/js/main.js'
+  	])
+  .pipe(sourcemaps.init())
+	.pipe(concat('main.min.js'))
+	.pipe(uglify())
+	.pipe(sourcemaps.write())
+  .pipe(gulp.dest('build/js'));
 });
  
  // Sprite
@@ -77,12 +96,13 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
 	gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
 	gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+	gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 //Default
 gulp.task('default', gulp.series(
 	'clean',
-	gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+	gulp.parallel('templates:compile', 'styles:compile', 'js', 'sprite', 'copy'),
 	gulp.parallel('watch', 'server')
 	)
 );
